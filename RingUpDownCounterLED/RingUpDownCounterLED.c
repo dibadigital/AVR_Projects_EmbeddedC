4aab881fdf97f4efde64f5f5d92c629775bdcc91
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-15 20:44:51 +0430

    Added all

---
+++ RingUpDownCounterLED/RingUpDownCounterLED.c
@@ -0,0 +1,52 @@
+#include <mega16.h>
+#include <delay.h>
+
+//void main(void)
+//{
+//    unsigned char i = 0;
+//    unsigned char select = 0;
+//    
+//    DDRB = 0xFF;
+//    PORTB = 0x00;
+//    
+//    while(1)
+//    {      
+//    
+//        if(select == 0)
+//        {
+//           PORTB = PORTB | (1 << i);
+//        }
+//        else
+//        {
+//           PORTB = 0xFE << i;
+//        }                    
+//        
+//        i++;
+//        if(i >= 8)
+//        {
+//            select = !select;
+//            i = 0;
+//        }  
+//        delay_ms(300);
+//    }
+//}
+
+void main(void)
+{
+    unsigned char i = 0;
+
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    
+    while(1)
+    {      
+        PORTB = PORTB ^ (1 << i);                 
+        
+        i++;
+        if(i >= 8)
+        {
+            i = 0;
+        }  
+        delay_ms(300);
+    }
+}
