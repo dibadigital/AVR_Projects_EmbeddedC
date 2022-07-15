c9212a58ed4a3ae36bc2e63cd24db1f04ec5d3dd
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-15 20:42:30 +0430

    Added all

---
+++ RingCounterLED/RingCounterLED.c
@@ -0,0 +1,21 @@
+#include <mega16.h>
+#include <delay.h>
+
+void main(void)
+{
+    unsigned char i = 0;
+    
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    
+    while(1)
+    {
+        PORTB = 1 << i;
+        i++;
+        if(i >= 8)  
+        {
+            i = 0;
+        }
+        delay_ms(300);    
+    }
+}
