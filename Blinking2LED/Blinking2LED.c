6f7efff483c167f289adcce1efdca13090dc6df4
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-15 20:24:57 +0430

    Add files via upload

---
+++ Blinking2LED/Blinking2LED.c
@@ -0,0 +1,16 @@
+#include <mega16.h>
+#include <delay.h>
+
+void main(void)
+{
+   DDRB = 0xFF;
+   PORTB = 0x00;
+   
+    while(1)
+    {
+        PORTB = 0x0F;
+        delay_ms(500); 
+        PORTB = 0xF0;
+        delay_ms(500);   
+    }
+}
