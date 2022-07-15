4b1cdc3ecd07abd72d8b825e0327ca3ceb298e29
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-15 20:39:03 +0430

    Added all

---
+++ UpCounterLED/UpCounterLED.c
@@ -0,0 +1,17 @@
+#include <mega16.h>
+#include <delay.h>
+
+void main(void)
+{
+    unsigned char i = 0;  //0 to 255
+    
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    
+    while(1)
+    {
+      i++;
+      PORTB = i;
+      delay_ms(300);  
+    }
+}
