6713c9762e981227ec9a26bd1f1f503685492a0f
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-15 20:48:29 +0430

    Added all

---
+++ ButtonTurnOnOffLED/ButtonTurnOnOffLED.c
@@ -0,0 +1,65 @@
+#include <mega16.h>
+#include <delay.h>
+
+//void main(void)
+//{
+//    DDRA = 0x00;
+//    PORTA = 0xFF;
+//    
+//    DDRB = 0xFF;
+//    PORTB = 0x00;
+//    
+//    while(1)
+//    {
+//        if(PINA.0 == 0)
+//        {
+//            PORTB = 0xFF;
+//        }
+//        else
+//        {
+//            PORTB = 0x00;
+//        }
+//    }
+//}
+
+//void main(void)
+//{
+//    DDRA = 0x00;
+//    PORTA = 0xFF;
+//    
+//    DDRB = 0xFF;
+//    PORTB = 0x00;
+//    
+//    while(1)
+//    {
+//        if((PINA & (1 << 0)) == 0)
+//        {
+//            PORTB = 0xFF;
+//        }
+//        else
+//        {
+//            PORTB = 0x00;
+//        }
+//    }
+//}
+
+void main(void)
+{
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    
+    while(1)
+    {
+        if(((PINA >> 0) & 0x01) == 0x00)
+        {
+            PORTB = 0xFF;
+        }
+        else
+        {
+            PORTB = 0x00;
+        }
+    }
+}
