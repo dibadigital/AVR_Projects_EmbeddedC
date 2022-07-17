5e681fad5ae3c4f98eb30b5aa1da1dbea4b785d6
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-17 12:32:27 +0430

    Update DownCounter2DigitsBCD7Segment.c

--- DownCounter2DigitsBCD7Segment/DownCounter2DigitsBCD7Segment.c
+++ DownCounter2DigitsBCD7Segment/DownCounter2DigitsBCD7Segment.c
@@ -1,22 +1,53 @@
 #include <mega16.h>
 #include <delay.h>
 
+//void main(void)
+//{
+//    signed char x = 99;
+//    unsigned char a = 0, b = 0;
+//    
+//    DDRC = 0xFF;
+//    PORTC = 0x00;
+//    
+//    while(1)
+//    {
+//       a = x/10;
+//       b = x%10;
+//       PORTC = (a << 4) | b;
+//       delay_ms(500);
+//       x--;
+//       if(x < 0)
+//        x = 99; 
+//    }
+//}
+
+unsigned char show_BCD(unsigned char x);
+
 void main(void)
 {
-    signed char x = 99;
-    unsigned char a = 0, b = 0;
+    signed char i = 99;
     
     DDRC = 0xFF;
     PORTC = 0x00;
     
     while(1)
     {
-       a = x/10;
-       b = x%10;
-       PORTC = (a << 4) | b;
+       PORTC = show_BCD(i);
        delay_ms(500);
-       x--;
-       if(x < 0)
-        x = 99; 
+       i--;
+       if(i< 0)
+        i = 99; 
     }
+}
+
+unsigned char show_BCD(unsigned char x)
+{
+    unsigned char a = 0, b = 0; 
+    unsigned char y = 0;
+    
+    a = x/10;  
+    b = x%10;  
+    y = (a << 4) | b;
+    
+    return y;
 }
