d4c2d34b768a0bae39900667a4e88a371fc1cf1a
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-19 19:16:22 +0430

    Added enum

--- DancingLight2LED/DancingLight2LED.c
+++ DancingLight2LED/DancingLight2LED.c
@@ -1,10 +1,88 @@
 #include <mega16.h>
 #include <delay.h>
 
+//void main(void)
+//{
+//    unsigned char x = 1, select = 0;
+//     
+//    DDRA = 0xFF;
+//    PORTA = 0x00;
+//    
+//    DDRB = 0xFF;
+//    PORTB = 0x00;
+//    
+//    DDRC = 0xFF;
+//    PORTC = 0x00;
+//    
+//    DDRD = 0xFF;
+//    PORTD = 0x00;
+//    
+//    while(1)
+//    {
+//       switch(select)
+//       {
+//            case 0:
+//                PORTA = x; 
+//                delay_ms(200);
+//                x = x << 1;
+//                if(x == 0)
+//                {
+//                    PORTA = 0;
+//                    select = 1;
+//                    x = 1;
+//                }
+//            break;
+//            
+//            case 1: 
+//                PORTB = x; 
+//                delay_ms(200);
+//                x = x << 1;
+//                if(x == 0)
+//                {
+//                    PORTB = 0;
+//                    select = 2;
+//                    x = 1;
+//                }
+//            break;
+//            
+//            case 2:    
+//                PORTC = x; 
+//                delay_ms(200);
+//                x = x << 1;
+//                if(x == 0)
+//                {
+//                    PORTC = 0;
+//                    select = 3;
+//                    x = 1;
+//                }
+//            break;
+//            
+//            case 3: 
+//                PORTD = x; 
+//                delay_ms(200);
+//                x = x << 1;
+//                if(x == 0)
+//                {
+//                    PORTD = 0;
+//                    select = 0;
+//                    x = 1;
+//                }
+//            break;
+//       }
+//    }
+//}
+
 void main(void)
 {
-    unsigned char x = 1, select = 0;
-     
+    unsigned char x = 1;
+    
+    enum s{
+        STATE_A,
+        STATE_B,
+        STATE_C,
+        STATE_D
+    }state;    
+    
     DDRA = 0xFF;
     PORTA = 0x00;
     
@@ -19,52 +97,52 @@ void main(void)
     
     while(1)
     {
-       switch(select)
+       switch(state)
        {
-            case 0:
+            case STATE_A:
                 PORTA = x; 
                 delay_ms(200);
                 x = x << 1;
                 if(x == 0)
                 {
                     PORTA = 0;
-                    select = 1;
+                    state = STATE_B;
                     x = 1;
                 }
             break;
             
-            case 1: 
+            case STATE_B: 
                 PORTB = x; 
                 delay_ms(200);
                 x = x << 1;
                 if(x == 0)
                 {
                     PORTB = 0;
-                    select = 2;
+                    state = STATE_C;
                     x = 1;
                 }
             break;
             
-            case 2:    
+            case STATE_C:    
                 PORTC = x; 
                 delay_ms(200);
                 x = x << 1;
                 if(x == 0)
                 {
                     PORTC = 0;
-                    select = 3;
+                    state = STATE_D;
                     x = 1;
                 }
             break;
             
-            case 3: 
+            case STATE_D: 
                 PORTD = x; 
                 delay_ms(200);
                 x = x << 1;
                 if(x == 0)
                 {
                     PORTD = 0;
-                    select = 0;
+                    state = STATE_A;
                     x = 1;
                 }
             break;
