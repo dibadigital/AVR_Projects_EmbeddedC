6f6beeb9e436d8fbf651e67d1a03f728c7ca1c1b
dibadigital <107550965+dibadigital@users.noreply.github.com>
2022-07-28 19:31:05 +0430

    Update keypad4x4.c

--- Keypad4X4LED/keypad4x4.c
+++ Keypad4X4LED/keypad4x4.c
@@ -11,28 +11,28 @@ unsigned char KeypadRead(void)
     unsigned char x = 16;
     
     R1 = 0; R2 = 1; R3 = 1; R4 = 1;
-    delay_ms(1);
+    delay_ms(1);  //Because half of the port is input and half of the port is output.
     if (C1 == 0){delay_ms(200); x = 7;}
     if (C2 == 0){delay_ms(200); x = 8;}
     if (C3 == 0){delay_ms(200); x = 9;}
     if (C4 == 0){delay_ms(200); x = 10;}
     
     R1 = 1; R2 = 0; R3 = 1; R4 = 1;
-    delay_ms(1);
+    delay_ms(1);  //Because half of the port is input and half of the port is output.
     if (C1 == 0){delay_ms(200); x = 4;}
     if (C2 == 0){delay_ms(200); x = 5;}
     if (C3 == 0){delay_ms(200); x = 6;}
     if (C4 == 0){delay_ms(200); x = 11;}
     
     R1 = 1; R2 = 1; R3 = 0; R4 = 1;
-    delay_ms(1);
+    delay_ms(1);  //Because half of the port is input and half of the port is output.
     if (C1 == 0){delay_ms(200); x = 1;}
     if (C2 == 0){delay_ms(200); x = 2;}
     if (C3 == 0){delay_ms(200); x = 3;}
     if (C4 == 0){delay_ms(200); x = 12;}
     
     R1 = 1; R2 = 1; R3 = 1; R4 = 0;
-    delay_ms(1);
+    delay_ms(1);  //Because half of the port is input and half of the port is output.
     if (C1 == 0){delay_ms(200); x = 14;}
     if (C2 == 0){delay_ms(200); x = 0;}
     if (C3 == 0){delay_ms(200); x = 15;}
